80b2b60bc569b0e535e13206f3973b632094063d
Aram47 <aram.minasyan.00@gmail.com>
2025-05-01 19:25:24 +0400

    [CHOR](add): add endpoint

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.use(cors());
 
 app.use('/api', router);
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server is running",
+  });
+});
+
 app.listen(process.env.PORT, process.env.HOST, () => {
   console.log(`Server is running on ${process.env.HOST}:${process.env.PORT}`);
 });
